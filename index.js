0173cb34bfd6c496deaeb6de4c16b2498919b739
Tomás Marquez <btomasmarquez@gmail.com>
2024-09-01 21:03:36 +0000

    connected db

--- index.js
+++ index.js
@@ -3,13 +3,42 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = process.env.PASS
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run();
+
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.get('/api/users', (req,res) => {
 
+})
 
 
 
