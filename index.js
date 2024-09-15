5f06bb210f40dbb893ebbd6300444091bb5d6ede
Tomás Marquez <btomasmarquez@gmail.com>
2024-09-15 21:58:10 +0000

    created first record using /api/users

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const mongoose= require('mongoose')
+
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded());
+
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -9,26 +16,34 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.PASS
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
+/*const client = new MongooseClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
   }
 });
+*/
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    await mongoose.connect(uri);
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+    }
 }
 run();
+
+const userSchema = new mongoose.Schema({
+username: String,
+description: String,
+duration: Number,
+date: Date
+})
+
+const User = mongoose.model('User', userSchema);
 
 app.use(cors())
 app.use(express.static('public'))
@@ -36,9 +51,18 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-app.get('/api/users', (req,res) => {
+app.post('/api/users', (req,res) => {
+const username = req.body.username
+
+async function run(){
+  await User.create({username: username})
+}
+run();
 
 })
+
+app.post('/api/users/:_id/exercises',(req,res) =>
+{})
 
 
 
